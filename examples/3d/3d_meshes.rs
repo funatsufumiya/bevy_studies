7b233539d9410523c7401bd51767cb02d92777d8
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2024-01-25 15:21:46 +0900

    add 3d meshes example

---
+++ examples/3d/3d_meshes.rs
@@ -0,0 +1,77 @@
+use bevy::prelude::*;
+use std::f32::consts::PI;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .add_systems(Update, rotate)
+        .run();
+}
+
+#[derive(Component)]
+struct Shape;
+
+const X_EXTENT: f32 = 14.5;
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    let debug_material = materials.add(StandardMaterial {
+        base_color: Color::rgb(0.75, 0.25, 0.25),
+        ..default()
+    });
+
+    let shapes = [
+        meshes.add(shape::Torus::default().into()),
+        meshes.add(shape::Box::default().into()),
+        meshes.add(shape::Cylinder::default().into()),
+    ];
+
+    let num_shapes = shapes.len();
+
+    for (i, shape) in shapes.into_iter().enumerate() {
+        commands.spawn((
+            PbrBundle {
+                mesh: shape,
+                material: debug_material.clone(),
+                transform: Transform::from_xyz(
+                    -X_EXTENT / 2. + i as f32 / (num_shapes - 1) as f32 * X_EXTENT,
+                    2.0,
+                    0.0,
+                )
+                .with_rotation(Quat::from_rotation_x(-PI / 4.)),
+                ..default()
+            },
+            Shape,
+        ));
+    };
+
+    commands.spawn(PointLightBundle {
+        point_light: PointLight {
+            intensity: 9000.0,
+            range: 100.,
+            shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform::from_xyz(8.0, 16.0, 8.0),
+        ..default()
+    });
+
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_xyz(0.0, 6., 12.0).looking_at(Vec3::new(0., 1., 0.), Vec3::Y),
+        camera: Camera {
+            order: 0,
+            ..default()
+        },
+        ..default()
+    });
+}
+
+fn rotate(mut query: Query<&mut Transform, With<Shape>>, time: Res<Time>) {
+    for mut transform in &mut query {
+        transform.rotate_y(time.delta_seconds() / 2.);
+    }
+}
