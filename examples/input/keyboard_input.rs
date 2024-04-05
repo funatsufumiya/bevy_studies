e0d122472a5410b748730fbdb90165f219be9c7b
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2024-04-05 16:34:31 +0900

    update example

--- examples/input/keyboard_input.rs
+++ examples/input/keyboard_input.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::*;
+use bevy::window::WindowMode;
 
 fn main() {
     App::new()
@@ -16,12 +17,30 @@ fn setup(
     mut commands: Commands,
     // mut meshes: ResMut<Assets<Mesh>>,
     // mut materials: ResMut<Assets<ColorMaterial>>,
+    // mut window: Query<&mut Window>,
 ) {
     commands.spawn(Camera2dBundle::default());
+
+    // let mut w = window.single_mut();
+    // w
+    //     .resolution
+    //     .set_scale_factor_override(Some(1.0));
+
+    // show sample sprite
+    commands.spawn(SpriteBundle {
+        sprite: Sprite {
+            color: Color::rgb(0.25, 0.25, 0.75),
+            custom_size: Some(Vec2::new(300.0, 400.0)),
+            ..default()
+        },
+        transform: Transform::from_translation(Vec3::new(-50., 0., 0.)),
+        ..default()
+    });
 }
 
 fn key_handler(
     keyboard_input: Res<Input<KeyCode>>,
+    mut window: Query<&mut Window>,
 ) {
     if keyboard_input.just_pressed(KeyCode::Space) {
         println!("Space key pressed");
@@ -34,5 +53,17 @@ fn key_handler(
     }
     if keyboard_input.just_pressed(KeyCode::Left) {
         println!("Left key pressed");
+    }
+    if keyboard_input.just_pressed(KeyCode::F) {
+        println!("F key pressed");
+        let mut w = window.single_mut();
+        const fs: WindowMode = WindowMode::BorderlessFullscreen;
+        // toggle fullscreen
+        if w.mode != fs {
+            w.mode = fs
+        } else {
+            w.mode = WindowMode::Windowed;
+        }
+        
     }
 }
