86ffe77fe433aa45c2b5bf88f7154273fae7011d
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2024-04-10 16:04:54 +0900

    add multi_fullscreen example

---
+++ examples/window/multi_fullscreen.rs
@@ -0,0 +1,123 @@
+use bevy::prelude::*;
+use bevy::window::{Cursor, PresentMode, WindowLevel, WindowResolution};
+use bevy::window::WindowMode;
+
+/// This example demonstrates how to show fullscreen window among multiple screens.
+/// example window resolution: 3840x1080
+
+const WIDTH: f32 = 3840.;
+const HEIGHT: f32 = 1080.;
+
+// // test: other resolutions
+// const WIDTH: f32 = 2560.;
+// const HEIGHT: f32 = 1440.;
+
+// const FULLSCREEN_MODE : WindowMode = WindowMode::BorderlessFullscreen;
+
+fn main() {
+    App::new()
+        .add_plugins(
+            DefaultPlugins.set(WindowPlugin {
+                primary_window: Some(Window {
+                    title: "fullscreen test".to_string(),
+//                    mode: FULLSCREEN_MODE, // set initial window mode here
+					present_mode: PresentMode::AutoNoVsync,
+					position: WindowPosition::At(IVec2::new(0, 0)),
+					resolution: WindowResolution::new(WIDTH, HEIGHT),
+					resizable: false,
+					decorations: false,
+					window_level: WindowLevel::AlwaysOnTop,
+					cursor: Cursor{visible:false,..default()},
+					
+                    ..default()
+                }),
+                ..default()
+            }),
+        )
+        .add_systems(Startup, setup)
+        .add_systems(Update, key_handler)
+        .add_systems(Update, bevy::window::close_on_esc)
+        .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    // mut meshes: ResMut<Assets<Mesh>>,
+    // mut materials: ResMut<Assets<ColorMaterial>>,
+    // mut window: Query<&mut Window>,
+) {
+    commands.spawn(Camera2dBundle::default());
+
+    commands.spawn(NodeBundle {
+        style: Style {
+            width: Val::Percent(100.),
+            height: Val::Percent(100.),
+            flex_direction: FlexDirection::Column,
+            justify_content: JustifyContent::Center,
+            align_items: AlignItems::Center,
+            ..default()
+        },
+        ..default()
+    })
+    .with_children(|root| {
+        root.spawn(
+            TextBundle::from_section(
+                "Multi-Fullscreen Test",
+                TextStyle {
+                    font_size: 50.0,
+                    color: Color::WHITE,
+                    ..default()
+                })
+                .with_text_alignment(TextAlignment::Center)
+                .with_style(Style {
+                    ..default()
+                })
+        );
+        // root.spawn(
+        //     TextBundle::from_section(
+        //         "* Press F to toggle fullscreen",
+        //         TextStyle {
+        //             font_size: 20.0,
+        //             color: Color::WHITE,
+        //             ..default()
+        //         })
+        //         .with_text_alignment(TextAlignment::Center)
+        //         .with_style(Style {
+        //             ..default()
+        //         })
+        // );
+        root.spawn(
+            TextBundle::from_section(
+                "* Press Esc to close",
+                TextStyle {
+                    font_size: 20.0,
+                    color: Color::WHITE,
+                    ..default()
+                })
+                .with_text_alignment(TextAlignment::Center)
+                .with_style(Style {
+                    ..default()
+                })
+        );
+    });
+}
+
+fn key_handler(
+    keyboard_input: Res<Input<KeyCode>>,
+    // keyboard_input: Res<ButtonInput<KeyCode>>, // for bevy 0.13
+    mut window: Query<&mut Window>,
+) {
+    // if keyboard_input.just_pressed(KeyCode::F) {
+    // // if keyboard_input.just_pressed(KeyCode::KeyF) { // for bevy 0.13
+    //     println!("F key pressed");
+    //     let mut w = window.single_mut();
+
+    //     // toggle fullscreen and windowed mode
+    //     if w.mode != FULLSCREEN_MODE {
+    //         w.mode = FULLSCREEN_MODE
+    //     } else {
+    //         w.mode = WindowMode::Windowed;
+    //     }
+        
+    // }
+}
