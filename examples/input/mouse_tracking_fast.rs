96e7453b71fc2fe40fc1ec28747ff59555af2746
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2024-04-20 07:39:32 +0900

    updated example dependencies

--- examples/input/mouse_tracking_fast.rs
+++ examples/input/mouse_tracking_fast.rs
@@ -3,29 +3,16 @@ use bevy::{
     prelude::*, sprite::MaterialMesh2dBundle, window::WindowMode
 };
 
-use mouse_position::mouse_position::Mouse;
+use bevy_quick_response::*;
 
 // Check mouse position in two different systems, and gizmos
 
 fn main() {
     App::new()
         // .add_plugins(DefaultPlugins)
-        .add_plugins(
-            DefaultPlugins.set(WindowPlugin {
-                primary_window: Some(Window {
-                    mode: WindowMode::BorderlessFullscreen,
-                    // present_mode: bevy::window::PresentMode::Mailbox,
-                    // present_mode: bevy::window::PresentMode::Immediate,
-                    // present_mode: bevy::window::PresentMode::Fifo,
-                    // present_mode: bevy::window::PresentMode::AutoNoVsync,
-                    ..default()
-                }),
-                ..default()
-            }),
-        )
+        .add_plugins(QuickResponsePlugin::default())
         .add_systems(Startup, setup)
         .add_systems(PreUpdate, mouse_tracking_a)
-        .add_systems(PreUpdate, mouse_tracking_b)
         .add_systems(Update, bevy::window::close_on_esc)
         .run();
 }
@@ -53,14 +40,6 @@ fn setup(
         transform: Transform::from_translation(Vec3::new(0., 0., 0.)),
         ..default()
     }, MouseTrackingSpriteA));
-
-    // Circle B
-    commands.spawn((MaterialMesh2dBundle {
-        mesh: meshes.add(Circle::new(30.)).into(),
-        material: materials.add(ColorMaterial::from(Color::rgba(1., 0., 0., 0.5))),
-        transform: Transform::from_translation(Vec3::new(0., 0., 0.1)),
-        ..default()
-    }, MouseTrackingSpriteB));
 }
 
 
@@ -80,27 +59,5 @@ fn mouse_tracking_a(
         // eprintln!("World coords: {}/{}", world_position.x, world_position.y);
         transform.translation.x = world_position.x;
         transform.translation.y = world_position.y;
-    }
-}
-
-fn mouse_tracking_b(
-    mut transforms: Query<(&mut Transform, &MouseTrackingSpriteB)>,
-    camera_q: Query<(&Camera, &GlobalTransform), With<MainCamera>>,
-    // windows: Query<&mut Window>,
-    mut gizmos: Gizmos,
-) {
-    // let window = windows.single();
-    let mut transform = transforms.single_mut().0;
-    let (camera, camera_transform) = camera_q.single();
-
-    let pos = Mouse::get_mouse_position();
-    if let Mouse::Position { x, y } = pos {
-        let cursor = Vec2::new(x as f32, y as f32);
-        let pos_world = camera.viewport_to_world_2d(camera_transform, cursor);
-        if let Some(world_position) = pos_world {
-            // eprintln!("World coords: {}/{}", world_position.x, world_position.y);
-            transform.translation.x = world_position.x;
-            transform.translation.y = world_position.y;
-        }
     }
 }
